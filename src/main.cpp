d58b07199068ceccea00e6f5398bee2549d61fa9
naoki-test1 <132342855+naoki-test1@users.noreply.github.com>
2025-10-30 13:56:33 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-/**
- * @ Author: zauberflote1
- * @ Create Time: 2024-10-11 03:33:44
- * @ Modified by: zauberflote1
- * @ Modified time: 2024-10-11 03:56:06
- * @ Description: BASIC MAIN LOOP FOR MAPPER_CMD
- */
-
 #include "mapper_cmd.hpp"
 
 std::atomic<bool> running(true);
